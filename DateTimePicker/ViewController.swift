971e6d1fe71a139bc963720e6399661200dac460
bre7 <mobileuy@gmail.com>
2016-10-26 00:40:28 -0300

    Set time interval to 10 in example

--- DateTimePicker/ViewController.swift
+++ DateTimePicker/ViewController.swift
@@ -11,17 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var item: UINavigationItem!
-    var current = Date()
+    var now = Date()
 
     @IBAction func showDateTimePicker(sender: AnyObject) {
-        let min = Date().addingTimeInterval(-60 * 60 * 24 * 365)
-        let max = Date().addingTimeInterval(60 * 60 * 24 * 365)
-        let picker = DateTimePicker.show(selected: self.current, minimumDate: min, maximumDate: max)
+        let min = now.addingTimeInterval(-60 * 60 * 24 * 365)
+        let max = now.addingTimeInterval(60 * 60 * 24 * 365)
+        let picker = DateTimePicker.show(selected: self.now, minimumDate: min, maximumDate: max, timeInterval: 10)
         picker.highlightColor = UIColor(red: 255.0/255.0, green: 138.0/255.0, blue: 138.0/255.0, alpha: 1)
         picker.doneButtonTitle = "!! DONE DONE !!"
         picker.todayButtonTitle = "Today👌Today"
         picker.completionHandler = { date in
-            self.current = date
+            self.now = date
             let formatter = DateFormatter()
             formatter.dateFormat = "HH:mm dd/MM/YYYY"
             self.item.title = formatter.string(from: date)
